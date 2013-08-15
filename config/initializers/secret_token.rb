a2e823cda145391c521d4cbaba38d99ff0536908
Andre Schweighofer <andre.schweighofer@xing.com>
2013-08-15 12:02:01 +0200

    empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studymatch::Application.config.secret_token = '03b6f555cb0233aa1ccb953aa5941c44c39a852d43b3918132dadb2db84054cdef829b1298a8a8ddcdac9bfc36231ad931cf4c5992d8d7d024df2b7f759ff3a8'
