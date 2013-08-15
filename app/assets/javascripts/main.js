849c7cabec7f1746df6e718219755a5c587638ad
Francois <webarbeit@gmail.com>
2013-08-15 15:49:06 +0200

    Improved look of navigation menu for mobile

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,43 @@
+function toggleModal(show) {
+
+	var modalObject = $('.modal');
+
+	if (show) {
+
+		// If already on the page, then only update size
+		if (modalObject.length) {
+
+			var modal = modalObject;
+
+		// Create and append modal to body
+		} else {
+			var modal = $('<div class="modal"></div>');
+			$('body').append(modal);
+		}
+
+		var windowObj = $(window);
+
+		modal.css({
+			width: windowObj.width(),
+			height: windowObj.height()
+		});
+
+	// Remove modal
+	} else {
+		modalObject.remove();
+	}
+
+};
+
+
 function toggleNavigation() {
-	$('.nav').toggle();
+
+	var navObj = $('.nav'),
+		className = 'slideTop';
+
+	navObj.toggleClass(className);
+
+	toggleModal(navObj.hasClass(className));
 };
 
 function hideNavigation() {
@@ -14,9 +52,6 @@ function bindDocumentEvents() {
 	menuIcon.tap(function() { toggleNavigation(); e.stopPropagation(); });
 	// Todo: onResize: check if > 900 and make nav visible
 
-	/*
-	$(document).click(function() { hideNavigation(); });
-	*/
 	$(document).tap(function() { hideNavigation(); });
 
 };
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130815101820) do
+ActiveRecord::Schema.define(version: 20130815102344) do
+
+  create_table "students", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "studies", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "votes", force: true do |t|
     t.datetime "created_at"
