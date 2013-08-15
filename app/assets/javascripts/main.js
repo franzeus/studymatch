6aba0f9cbe92bb0094bbbbc9b603594c52bca891
Francois <webarbeit@gmail.com>
2013-08-15 20:01:23 +0200

    Fixed taping on nav link triggers link

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -49,12 +49,9 @@ function bindDocumentEvents() {
 
 	/* Bind click and tap event for mobile collapse navigation */
 	var menuIcon = $('.menuIcon');
-	menuIcon.on('click', function(e) { toggleNavigation(); e.stopPropagation(); });
-	menuIcon.tap(function() { toggleNavigation(); e.stopPropagation(); });
+	//menuIcon.on('click', function(e) { toggleNavigation(); e.stopPropagation(); });
+	menuIcon.tap(function(e) { toggleNavigation(); e.stopPropagation(); });
 	// Todo: onResize: check if > 900 and make nav visible
-
-	$(document).tap(function() { hideNavigation(); });
-
 };
 
 $(document).ready(function() {
