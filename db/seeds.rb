09172327a58f904ce3397e7ab250e5592d281ee3
Andre Schweighofer <andre.schweighofer@xing.com>
2013-08-17 17:26:35 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+    mmt = Study.create(title: 'Multimedia Technology')
+    mma = Study.create(title: 'Multimedia Art')
+    Study.create(title: 'Medicine')
+
+    Student.create(email: 'fake@boobs.cum', study: mmt, external_id: '588380875')
+    Student.create(email: 'mail@fail.lol', study: mma, external_id: '742197780')
